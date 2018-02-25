e2e76bc1e28812a8dc0eca005db6fa397c8d0792
pboedker <peter@boedker.dk>
2018-02-25 22:55:46 +0100

    First files uploaded to repository.
    After some hacking and testing and general time wasting (procrastination) while building the drive train, I'm now ready for development of the ECU.

---
+++ DefineIO.h
@@ -0,0 +1,22 @@
+// Declaring pin numbers for IO
+int Ain0_var =      A0; // Not used
+int AinKey =        A1; // Input pin for the keyboard encoding
+int AinBatI =       A2; // Input pin for the battery current
+int AinBatV =       A3; // Input pin for the battery voltage
+int Ain4_var =      A4; // Reserved for I2C-SDA for 4x20 LCD Display
+int Ain5_var =      A5; // Reserved for I2C-SCL for 4x20 LCD Display
+int D0_var =         0; // ? Reserved for Serial Rx?
+int D1_var =         1; // ? Reserved for Serial Tx?
+int D2_var =         2; // Not used (INT0) (use this for speed sensor?)
+int D3_var =         3; // Not used (INT1, PWM)
+int D4_var =         4; // Not used
+int D5_var =         5; // Not used (PWM)
+int D6_var =         6; // Not used (PWM)
+int DinDrive =       7; // Gear selector, Drive
+int DinReverse =     8; // Gear selector, Reverse
+int D9_var =         9; // Not used (PWM)
+int D10_var =       10; // Not used (PWM)
+int D11_var =       11; // Not used (PWM)
+int D12_var =       12; // Not used
+int D13_var =       13; // Not used (LED)
+
