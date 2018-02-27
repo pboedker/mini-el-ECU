97d7b349653c19c94cf47e96f7598973506b866e
pboedker <peter@boedker.dk>
2018-02-27 22:13:09 +0100

    Updating the IO defines...

--- DefineIO.h
+++ DefineIO.h
@@ -1,22 +1,32 @@
 // Declaring pin numbers for IO
-int Ain0_var =      A0; // Not used
+int Ain0_var =      A0; // - Not used
 int AinKey =        A1; // Input pin for the keyboard encoding
 int AinBatI =       A2; // Input pin for the battery current
-int AinBatV =       A3; // Input pin for the battery voltage
-int Ain4_var =      A4; // Reserved for I2C-SDA for 4x20 LCD Display
-int Ain5_var =      A5; // Reserved for I2C-SCL for 4x20 LCD Display
-int D0_var =         0; // ? Reserved for Serial Rx?
-int D1_var =         1; // ? Reserved for Serial Tx?
-int D2_var =         2; // Not used (INT0) (use this for speed sensor?)
+int AinBatV =       A3; // Input pin for the full battery voltage
+int Ain4_var =      A4; // I2C-SDA for 4x20 LCD Display
+int Ain5_var =      A5; // I2C-SCL for 4x20 LCD Display
+int D0_var =         0; // - Reserved for Serial Rx?
+int D1_var =         1; // - Reserved for Serial Tx?
+int DinSpeed =       2; // Speed sensor (INT0)
 int D3_var =         3; // Not used (INT1, PWM)
-int D4_var =         4; // Not used
-int D5_var =         5; // Not used (PWM)
-int D6_var =         6; // Not used (PWM)
+int DinBeam =        4; // Beam (stalk)
+int DoutFlashLeft =  5; // Flasher, Left (also PWM)
+int DoutFlashRight = 6; // Flasher, Right (also PWM)
 int DinDrive =       7; // Gear selector, Drive
 int DinReverse =     8; // Gear selector, Reverse
-int D9_var =         9; // Not used (PWM)
-int D10_var =       10; // Not used (PWM)
-int D11_var =       11; // Not used (PWM)
-int D12_var =       12; // Not used
-int D13_var =       13; // Not used (LED)
+int DinFlashLeft =   9; // Flasher, Left (stalk) (also PWM)
+int DinFlashRight = 10; // Flasher, Right (stalk) (also PWM)
+int DoutWiper =     11; // Windscreen wiper (also PWM)
+int DoutWasher =    12; // Windscreen washer
+int DoutFan =       13; // Windscreen fan (LED)
+
+// Reserving relay functions (not SW)
+// 1: 
+// 2: 
+// 3: 
+// 4: 
+// 5: 
+// 6: Flash Right
+// 7: Flash Left
+// 8: High Beam 
 
